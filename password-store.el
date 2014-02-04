27217673feaff45ff2bdf3cca3154451f4b430da
Svend Sorensen <ssorensen@whitepages.com>
2014-02-04 11:54:54 -0800

    Use user-error instead of error

--- password-store.el
+++ password-store.el
@@ -93,6 +93,6 @@ avoid sending a password to the browser."
     (if (or (string-prefix-p "http://" url)
 	    (string-prefix-p "https://" url))
 	(browse-url url)
-      (error "%s" "String does not look like a URL"))))
+      (user-error "%s" "String does not look like a URL"))))
 
 ;;; password-store.el ends here
