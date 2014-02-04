e75e3a638e99f7b924ffdab9e48e5ad063be6634
Svend Sorensen <ssorensen@whitepages.com>
2014-02-04 11:52:35 -0800

    Use (error "%s" "string") as recommended by elisp docs

--- password-store.el
+++ password-store.el
@@ -93,6 +93,6 @@ avoid sending a password to the browser."
     (if (or (string-prefix-p "http://" url)
 	    (string-prefix-p "https://" url))
 	(browse-url url)
-      (error "String does not look like a URL"))))
+      (error "%s" "String does not look like a URL"))))
 
 ;;; password-store.el ends here
